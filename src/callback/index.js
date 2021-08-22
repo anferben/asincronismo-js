24187fbeb5ca769fdc382d5c64b42d824b397d7d
Andres <anferben@gmail.com>
2021-08-22 15:21:12 -0600

    first commit

---
+++ src/callback/index.js
@@ -0,0 +1,27 @@
+/*La estructura consite de una funcion que recibe
+como argumento otra funcion*/
+
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+function calc(num1, num2, callback) {
+    return callback(num1, num2);
+}
+
+console.log(calc(6, 2, sum));
+
+
+function date(callback) {
+    console.log("Fecha de date: " + new Date);
+    setTimeout(function () {
+        let date = new Date;
+        callback(date);
+    }, 3000)
+}
+
+function printDate(dateNow) {
+    console.log("Fecha de printDate: " + dateNow);
+}
+
+date(printDate);
