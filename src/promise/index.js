8cff96503e4a6ca372289e2761593e467bfad39a
Andres <anferben@gmail.com>
2021-09-01 22:00:47 -0600

    promises done

---
+++ src/promise/index.js
@@ -0,0 +1,39 @@
+const somethingWillHappen = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            resolve('Hey!');
+        } else {
+            reject('Ooops!');
+        }
+    });
+};
+
+somethingWillHappen()
+    .then(response => console.log(response))
+    .catch(error => console.error(error));
+
+const somethingWillHappen2 = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            setTimeout(() => {
+                resolve('True')
+            }, 2000);
+        } else {
+            const error = new Error('Oooops!');
+            reject(error);
+        }
+    });
+};
+
+somethingWillHappen2()
+    .then(response => console.log(response))
+    .catch(error => console.error(error));
+
+//Correr varias promesas encadenadas
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+    .then(response => {
+        console.log('Array of results', response);
+    })
+    .catch(error => {
+        console.error(error);
+    })
